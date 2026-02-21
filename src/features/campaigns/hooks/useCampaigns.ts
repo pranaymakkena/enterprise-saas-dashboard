2fdb06309e3af965607e5a743c340eadacc70b39
Pranay Makkena <pranaymakkena@gmail.com>
2026-02-21 17:50:44 +0530

    Update useCampaigns.ts

--- src/features/campaigns/hooks/useCampaigns.ts
+++ src/features/campaigns/hooks/useCampaigns.ts
@@ -5,11 +5,23 @@ export const useCampaigns = (
   page: number,
   search: string,
   sortKey: "name" | "budget" | "status" | null,
-  sortOrder: "asc" | "desc"
+  sortOrder: "asc" | "desc",
+  filters: {
+    status?: string;
+    minBudget?: number;
+    maxBudget?: number;
+  }
 ) =>
   useQuery({
-    queryKey: ["campaigns", page, search, sortKey, sortOrder],
+    queryKey: ["campaigns", page, search, sortKey, sortOrder, filters],
     queryFn: () =>
-      campaignService.getCampaigns(page, 10, search, sortKey, sortOrder),
+      campaignService.getCampaigns(
+        page,
+        10,
+        search,
+        sortKey,
+        sortOrder,
+        filters
+      ),
     keepPreviousData: true
   });
