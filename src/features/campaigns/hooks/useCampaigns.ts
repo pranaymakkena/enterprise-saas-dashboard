2144f586d32ac09d6a3c08e33d8ef8aaf27ac726
Pranay Makkena <pranaymakkena@gmail.com>
2026-02-21 17:43:06 +0530

    Update useCampaigns.ts

--- src/features/campaigns/hooks/useCampaigns.ts
+++ src/features/campaigns/hooks/useCampaigns.ts
@@ -1,9 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { campaignService } from "../services/campaignService";
 
-export const useCampaigns = (page: number, search: string) =>
+export const useCampaigns = (
+  page: number,
+  search: string,
+  sortKey: "name" | "budget" | "status" | null,
+  sortOrder: "asc" | "desc"
+) =>
   useQuery({
-    queryKey: ["campaigns", page, search],
-    queryFn: () => campaignService.getCampaigns(page, 10, search),
+    queryKey: ["campaigns", page, search, sortKey, sortOrder],
+    queryFn: () =>
+      campaignService.getCampaigns(page, 10, search, sortKey, sortOrder),
     keepPreviousData: true
   });
